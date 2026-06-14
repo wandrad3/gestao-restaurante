62fb4c541178c3662fb3bff251aad8e6c6571297
Your Name <wesleyandrade298@gmail.com>
2026-06-14 13:55:15 -0300

    feat: initial commit

---
+++ src/test/java/com/fiap/gestaorestaurante/GestaoRestauranteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fiap.gestaorestaurante;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestaoRestauranteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
